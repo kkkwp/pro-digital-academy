05d7c6e57408c1c75ef6887cca7f0b3dbae6ca96
kkkwp <chxrryda@gmail.com>
2023-08-08 10:29:37 +0900

    IIFE(즉시실행함수)

---
+++ javascript-class/day03/js/IIFE.js
@@ -0,0 +1,13 @@
+// IIFE: 정의하자마자 즉시 실행되는 함수
+(function prt() {
+    console.log('prt!!!');
+})();
+
+let fnprt = function (a, b) {
+    console.log(`test fnprt ${a}, ${b}`);
+};
+fnprt(10, 20);
+
+((a, b) => {
+    console.log(`test fnprt ${a}, ${b}`);
+})(10, 20);
