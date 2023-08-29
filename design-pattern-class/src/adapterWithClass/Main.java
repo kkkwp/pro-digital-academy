d81321b8f04400308458b20f7b55c0209bbfffca
kkkwp <chxrryda@gmail.com>
2023-08-29 10:44:38 +0900

    Class를 이용한 Adapter 패턴

---
+++ design-pattern-class/src/adapterWithClass/Main.java
@@ -0,0 +1,11 @@
+package adapterWithClass;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Print p = new PrintBanner("Hello");
+		p.printWeak();
+		p.printStrong();
+	}
+}
---
+++ design-pattern-class/src/adapterWithClass/Print.java
@@ -0,0 +1,8 @@
+package adapterWithClass;
+
+public interface Print {
+	
+	public abstract void printWeak();
+
+	public abstract void printStrong();
+}
