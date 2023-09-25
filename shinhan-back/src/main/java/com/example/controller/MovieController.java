83f6894f33e5216d91469a301eb2a38ee5376bb6
kkkwp <chxrryda@gmail.com>
2023-09-25 14:44:38 +0900

    api: @PathVariable

--- shinhan-back/src/main/java/com/example/controller/MovieController.java
+++ shinhan-back/src/main/java/com/example/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,13 @@ public class MovieController {
 	@GetMapping("/movies")
 	public String get() {
 		return "test";
+	}
+
+	@GetMapping("/movies/{movieId}")
+	public String getMovie(
+		@PathVariable(value = "movieId") long movieId
+	) {
+		return String.valueOf(movieId);
 	}
 
 	@PostMapping("/movies")
