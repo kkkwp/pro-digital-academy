b99ff4efaab177ec8dc661dc95b6667f0cec1bc2
kkkwp <chxrryda@gmail.com>
2023-09-25 15:19:28 +0900

    api: @Deprecated

--- shinhan-back/src/main/java/com/example/controller/MovieController.java
+++ shinhan-back/src/main/java/com/example/controller/MovieController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,9 +18,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RequestMapping("/api")
 public class MovieController {
 
+	@Deprecated(since = "v2 사용하세요.")
 	@GetMapping("/v1/movies")
 	public String get() {
 		return "test";
+	}
+
+	@GetMapping("/v2/movies")
+	public List<String> getmovies() {
+		return List.of("test");
 	}
 
 	@GetMapping("/v1/movies/{movieId}")
