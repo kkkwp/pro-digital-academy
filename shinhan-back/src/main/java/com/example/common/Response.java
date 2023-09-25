d6dcd435132e11bf6582648ba2574a231d42fe94
kkkwp <chxrryda@gmail.com>
2023-09-26 08:04:46 +0900

    refactor: getter 이용

--- shinhan-back/src/main/java/com/example/common/Response.java
+++ shinhan-back/src/main/java/com/example/common/Response.java
@@ -1,15 +1,14 @@
 package com.example.common;
 
+import lombok.Getter;
+
+@Getter
 public class Response<T> {
 
 	private final T data;
 
 	public Response(T data) {
 		this.data = data;
-	}
-
-	public T getData() {
-		return data;
 	}
 
 	public static <T> Response<T> of(T data) {
