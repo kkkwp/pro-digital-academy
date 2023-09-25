718ed740a2c91100514b5bf462c9277d076eaaa9
kkkwp <chxrryda@gmail.com>
2023-09-25 15:14:33 +0900

    api: @RequestMapping("/api") 추가

--- shinhan-back/src/main/java/com/example/controller/MovieController.java
+++ shinhan-back/src/main/java/com/example/controller/MovieController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +13,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "movie controller")
 @RestController
+@RequestMapping("/api")
 public class MovieController {
 
-	@GetMapping("/movies")
+	@GetMapping("/v1/movies")
 	public String get() {
 		return "test";
 	}
 
-	@GetMapping("/movies/{movieId}")
+	@GetMapping("/v1/movies/{movieId}")
 	public String getMovie(
 		@PathVariable(value = "movieId") long movieId,
 		@RequestParam(value = "v", required = false) Integer v
@@ -27,15 +29,15 @@ public class MovieController {
 		return String.valueOf(movieId + " " + v);
 	}
 
-	@PostMapping("/movies")
+	@PostMapping("/v1/movies")
 	public void create() {
 	}
 
-	@PutMapping("/movies")
+	@PutMapping("/v1/movies")
 	public void update() {
 	}
 
-	@DeleteMapping("/movies")
+	@DeleteMapping("/v1/movies")
 	public void delete() {
 	}
 }
