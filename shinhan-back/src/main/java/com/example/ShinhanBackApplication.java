e248d350a56788899fd679d72c9dd8afa566609c
kkkwp <chxrryda@gmail.com>
2023-09-25 13:09:58 +0900

    Bean: singleton으로 관리하기

--- shinhan-back/src/main/java/com/example/ShinhanBackApplication.java
+++ shinhan-back/src/main/java/com/example/ShinhanBackApplication.java
@@ -3,18 +3,29 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.ioc.Chef;
-import com.example.ioc.IngredientFactory;
+import com.example.config.MyConfig;
+import com.example.config.MyConfigProto;
 
 @SpringBootApplication
 public class ShinhanBackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShinhanBackApplication.class, args);
+		MyConfig myConfig1 = new MyConfig();
+		MyConfig myConfig2 = new MyConfig();
+		MyConfig myConfig3 = new MyConfig();
 
-		IngredientFactory ingredientFactory = new IngredientFactory();
-		Chef chef = new Chef(ingredientFactory);
-		String food = chef.cook("치킨");
-		System.out.println(food);
+		MyConfigProto myConfigProto1 = new MyConfigProto();
+		MyConfigProto myConfigProto2 = new MyConfigProto();
+		MyConfigProto myConfigProto3 = new MyConfigProto();
+
+		System.out.println(myConfig1);
+		System.out.println(myConfig2);
+		System.out.println(myConfig3);
+
+		System.out.println(myConfigProto1);
+		System.out.println(myConfigProto2);
+		System.out.println(myConfigProto3);
+
+		SpringApplication.run(ShinhanBackApplication.class, args);
 	}
 }
--- shinhan-back/src/main/java/com/example/ioc/PrintBeans.java
+++ shinhan-back/src/main/java/com/example/ioc/PrintBeans.java
@@ -1,11 +1,11 @@
 package com.example.ioc;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import com.example.config.MyConfig;
+import com.example.config.MyConfigProto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,9 +17,20 @@ public class PrintBeans {
 
 	@Bean
 	public void printBeansMethod() {
-		List<String> beans = Arrays.stream(applicationContext.getBeanDefinitionNames()).toList();
-		for (String bean : beans) {
-			System.out.println(bean);
-		}
+		MyConfig myConfig1 = applicationContext.getBean(MyConfig.class);
+		MyConfig myConfig2 = applicationContext.getBean(MyConfig.class);
+		MyConfig myConfig3 = applicationContext.getBean(MyConfig.class);
+
+		MyConfigProto myConfigProto1 = applicationContext.getBean(MyConfigProto.class);
+		MyConfigProto myConfigProto2 = applicationContext.getBean(MyConfigProto.class);
+		MyConfigProto myConfigProto3 = applicationContext.getBean(MyConfigProto.class);
+
+		System.out.println(myConfig1);
+		System.out.println(myConfig2);
+		System.out.println(myConfig3);
+
+		System.out.println(myConfigProto1);
+		System.out.println(myConfigProto2);
+		System.out.println(myConfigProto3);
 	}
 }
