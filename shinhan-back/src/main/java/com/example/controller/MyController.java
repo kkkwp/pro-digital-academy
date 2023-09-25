2c6ad3d10b3e7cdf6a08a5547657d5a4f9365287
kkkwp <chxrryda@gmail.com>
2023-09-25 11:48:55 +0900

    Bean: service, config, controller 생성

---
+++ shinhan-back/src/main/java/com/example/controller/MyController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.config.MyConfig;
+import com.example.service.MyService;
+
+@RestController
+public class MyController {
+
+	private final MyService myService;
+	private final MyConfig myConfig;
+
+	@Autowired
+	public MyController(MyService myService, MyConfig myConfig) {
+		this.myService = myService;
+		this.myConfig = myConfig;
+	}
+}
