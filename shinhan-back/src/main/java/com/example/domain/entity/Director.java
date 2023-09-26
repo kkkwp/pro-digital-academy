ff576bfd405a3202b0d332aae902f54184eafcb1
kkkwp <chxrryda@gmail.com>
2023-09-26 16:10:33 +0900

    JPA: director - @OnetoOne

--- shinhan-back/src/main/java/com/example/domain/entity/Director.java
+++ shinhan-back/src/main/java/com/example/domain/entity/Director.java
@@ -4,22 +4,19 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "movie")
+@Table(name = "director")
 @Getter
 @NoArgsConstructor
-public class Movie {
+public class Director {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,18 +25,6 @@ public class Movie {
 	@Column(name = "name")
 	private String name;
 
-	@Column(name = "production_year")
-	private int productionYear;
-
 	@Column(name = "created_at")
-	private LocalDateTime createdAt = LocalDateTime.now();
-
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "director_id")
-	private Director director;
-
-	public Movie(String name, int productionYear) {
-		this.name = name;
-		this.productionYear = productionYear;
-	}
+	private LocalDateTime createdAt;
 }
