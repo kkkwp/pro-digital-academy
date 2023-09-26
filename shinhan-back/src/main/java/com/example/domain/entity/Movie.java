2eba05de85385636b51930dd4e9f858a87040095
kkkwp <chxrryda@gmail.com>
2023-09-26 11:46:21 +0900

    JPA: Movie를 jpa를 이용해서 매핑

--- shinhan-back/src/main/java/com/example/domain/entity/Movie.java
+++ shinhan-back/src/main/java/com/example/domain/entity/Movie.java
@@ -2,17 +2,30 @@ package com.example.domain.entity;
 
 import java.time.LocalDateTime;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Getter;
+
+@Entity
+@Table(name = "movie")
 @Getter
-@Setter
-@AllArgsConstructor
 public class Movie {
 
-	private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name")
 	private String name;
-	private Integer productionYear;
-	private LocalDateTime createAt;
+
+	@Column(name = "production_year")
+	private int productionYear;
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
 }
